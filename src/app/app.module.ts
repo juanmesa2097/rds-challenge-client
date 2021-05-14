793ffa798d2d16d6a924adda1d703f26611dc3e3
juanmesa2097 <juanmesa2097@gmail.com>
2021-05-13 22:55:41 -0500

    feat: create header & layout modules + refactor styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './components/layout/layout.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
